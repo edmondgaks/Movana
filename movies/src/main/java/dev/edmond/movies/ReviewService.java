45d243af16288ab418c32c5146a2bf8b4f246ac9
edmondgaks <edmondgaks@gmail.com>
2023-11-27 11:30:58 +0200

    fixing the error and also starting frontend

--- movies/src/main/java/dev/edmond/movies/ReviewService.java
+++ movies/src/main/java/dev/edmond/movies/ReviewService.java
@@ -1,16 +1,31 @@
 package dev.edmond.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final MongoTemplate mongoTemplate;
     @Autowired
-    private ReviewRepository reviewRepository;
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
+        this.reviewRepository = reviewRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
     public Review createReview(String reviewBody, String imdbId) {
         Review review = new Review(reviewBody);
         reviewRepository.insert(review);
-        return  review;
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewId").value(review))
+                .first();
+
+        return review;
     }
 }
